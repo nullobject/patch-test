27703cc4d8bc5d1fcfa710743f39c2d8b5783f97
Joshua Bassett <hello@joshbassett.info>
2022-08-08 15:02:59 +1000

    Flash LED

--- main.cpp
+++ main.cpp
@@ -50,5 +50,10 @@ int main(void) {
   patch.StartAudio(AudioCallback);
 
   /** Loop forever */
-  while(1) {}
+  while(1) {
+    patch.WriteCvOut(CV_OUT_2, 5);
+    patch.Delay(1000);
+    patch.WriteCvOut(CV_OUT_2, 0);
+    patch.Delay(1000);
+  }
 }
